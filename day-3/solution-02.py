451668fe5cb10a8603759c98d0a95744767b4dfa
dstoecklein <daniel.stoecklein@gmail.com>
2023-12-09 16:56:12 +0100

    day 3 - part 2

--- day-3/solution-02.py
+++ day-3/solution-02.py
@@ -1,88 +1,94 @@
 import re
 
-PATTERN2 = r'\d+'
-PATTERN5 = r'(\d+)\*(\d+)'
-PATTERN6 = r'(\d+)\*'
-PATTERN7 = r'\*(\d+)'
-PATTERN8 = r'(\d+)'
+PATTERN = r"\d+"
 
-sum = 0
+
+def go_forwards(line, start=0):
+    part_number = ""
+    for c in line[start:]:
+        if c.isdigit():
+            part_number = part_number + c
+        else:
+            break
+    return part_number
+
+
+def go_backwards(line, end=0):
+    part_number = ""
+    for c in reversed(line[:end]):
+        if c.isdigit():
+            part_number = part_number + c
+        else:
+            break
+    return part_number[::-1]
+
 
 with open("input.txt") as f:
     lines = f.readlines()
 
+    sum = 0
+
     for line_index, curr_line in enumerate(lines):
-        curr_line = ''.join(curr_line)
-        adjacents = 0
+        curr_line = "".join(curr_line.strip())
 
         if line_index == 0:
             prev_line = curr_line
-        if line_index >= len(lines)-1:
+        if line_index >= len(lines) - 1:
             next_line = curr_line
         else:
-            next_line = lines[line_index+1]
+            next_line = lines[line_index + 1]
 
         for pos, c in enumerate(curr_line):
-
             if c == "*":
-                
                 adjacents = 0
-                
-                if curr_line[pos-1].isdigit():
+
+                # check number of adjacents before doing anything
+                if curr_line[pos - 1].isdigit():
                     adjacents += 1
-                if curr_line[pos+1].isdigit():
+                if curr_line[pos + 1].isdigit():
                     adjacents += 1
-                adjacents += len(re.findall(PATTERN2, prev_line[pos-1:pos+2]))
-                adjacents += len(re.findall(PATTERN2, next_line[pos-1:pos+2]))      
-                
+                adjacents += len(re.findall(PATTERN, prev_line[pos - 1:pos + 2]))
+                adjacents += len(re.findall(PATTERN, next_line[pos - 1:pos + 2]))
+
                 if adjacents != 2:
                     continue
 
-                ratios = list()
+                part_numbers = list()
 
-                star_has_two_neighbours = re.search(PATTERN5, curr_line)
-                if star_has_two_neighbours:
-                    ratios.append(int(star_has_two_neighbours.group(1)))
-                    ratios.append(int(star_has_two_neighbours.group(2)))
+                # part numbers next to *
+                if curr_line[pos - 1].isdigit():
+                    part_numbers.append(go_backwards(curr_line, end=pos))
+                if curr_line[pos + 1].isdigit():
+                    part_numbers.append(go_forwards(curr_line, start=pos + 1))
+
+                # part number in prev line at * position
+                if prev_line[pos].isdigit():
+                    part_numbers.append(
+                        go_backwards(prev_line, end=pos + 1)
+                        + go_forwards(prev_line, start=pos + 1)
+                    )
+                # part number in prev line at position -1 or +1 of *
                 else:
-                    if curr_line[pos-1].isdigit():
-                        star_has_left_neighbour = re.search(PATTERN6, curr_line[:pos+1])
-                        ratios.append(int(star_has_left_neighbour.group(1)))
-                    if curr_line[pos+1].isdigit():
-                        star_has_right_neighbour = re.search(PATTERN7, curr_line[pos:])
-                        ratios.append(int(star_has_right_neighbour.group(1)))
+                    if prev_line[pos - 1].isdigit():
+                        part_numbers.append(go_backwards(prev_line, end=pos))
+                    if prev_line[pos + 1].isdigit():
+                        part_numbers.append(go_forwards(prev_line, start=pos + 1))
 
-                if re.search(PATTERN8, prev_line) is not None:
-                    matches_prev = re.finditer(PATTERN2, prev_line)
-                    
-                    for match in matches_prev:
-                        digits = match.group()
-                        if pos == match.start():
-                            ratios.append(int(digits))
-                            
-                        if pos-1 in range(match.start(), match.end()):
-                            ratios.append(int(digits))
-                            
-                        if pos+1 in range(match.start(), match.end()+1):
-                            ratios.append(int(digits))
-                            
+                # part number in next line at * position
+                if next_line[pos].isdigit():
+                    part_numbers.append(
+                        go_backwards(next_line, end=pos + 1)
+                        + go_forwards(next_line, start=pos + 1)
+                    )
+                # part number in next line at position -1 or +1 of *
+                else:
+                    if next_line[pos - 1].isdigit():
+                        part_numbers.append(go_backwards(next_line, end=pos))
+                    if next_line[pos + 1].isdigit():
+                        part_numbers.append(go_forwards(next_line, start=pos + 1))
 
-                if re.search(PATTERN8, next_line) is not None:
-                    matches_next = re.finditer(PATTERN2, next_line)
-                    
-                    for match in matches_next:
-                        digits = match.group()
-                        if pos == match.start():
-                            ratios.append(int(digits))
-                            
-                        if pos-1 in range(match.start(), match.end()):
-                            ratios.append(int(digits))
-                            
-                        if pos+1 in range(match.start(), match.end()+1):
-                            ratios.append(int(digits))
+                sum += int(part_numbers[0]) * int(part_numbers[1])
 
-                sum += ratios[0] * ratios[1]
-                
         prev_line = curr_line
 
 print(sum)
