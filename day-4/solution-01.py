defc4f2edd99c786a3688320bb5d4bc4bb572177
dstoecklein <daniel.stoecklein@gmail.com>
2023-12-09 19:14:22 +0100

    day 4 - part 1

---
+++ day-4/solution-01.py
@@ -0,0 +1,21 @@
+import re
+
+PATTERN = r"Card\s+(\d+):\s+"
+DIGITS = r"\d+"
+
+sum = 0
+
+with open("input.txt") as f:
+    lines = f.readlines()
+
+    for line in lines:
+        card_numbers = re.sub(PATTERN, "", line.strip()).split("|")
+
+        winning_numbers = re.findall(DIGITS, card_numbers[0])
+        my_numbers = re.findall(DIGITS, card_numbers[1])
+        matching_numbers = set(winning_numbers) & set(my_numbers)
+        matching_numbers_count = len(matching_numbers)
+
+        sum += 2 ** (matching_numbers_count - 1) if matching_numbers_count >= 1 else 0
+
+print(sum)
